3181f03ecb86ac02b560aeecc422cacb04ba88c4
Josh McHugh <joshua.mchugh01@gmail.com>
2024-08-08 21:36:21 -0400

    Added WebView to the application and pointed to local server

--- src/main/scala/net/sailware/resumewizardfx/Application.scala
+++ src/main/scala/net/sailware/resumewizardfx/Application.scala
@@ -5,17 +5,23 @@ import jakarta.enterprise.event.Observes
 import io.quarkiverse.fx.FxPostStartupEvent
 import javafx.scene.Scene
 import javafx.scene.control.Label
-import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+import javafx.scene.layout.Priority
+import javafx.scene.web.WebView
 
 @ApplicationScoped
 class Application:
 
   def start(@Observes event: FxPostStartupEvent): Unit =
-    val javaVersion = System.getProperty("java.version")
-    val javafxVersion = System.getProperty("javafx.version")
-    val label = Label(s"Hello JavaFX $javafxVersion, running on Java ${javaVersion}.")
-    val scene = Scene(StackPane(label), 640, 480)
+    val webView = WebView()
+    val webEngine = webView.getEngine()
+    webEngine.load("http://localhost:8080/wizard/detail")
+    VBox.setVgrow(webView, Priority.ALWAYS)
+    val root = VBox()
+    root.getChildren().add(webView)
+    val scene = Scene(root)
     val stage = event.getPrimaryStage()
     stage.setScene(scene)
+    stage.setResizable(true)
     stage.show()
     
